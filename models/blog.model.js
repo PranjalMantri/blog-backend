b54d646a21e0d3b360e0a12b5a96dc3dc49a4b22
Pranjal Mantri <pranjalmantri@gmail.com>
2025-01-02 09:44:43 +0530

    feat: add blog schema

---
+++ models/blog.model.js
@@ -0,0 +1,29 @@
+import mongoose, { mongo } from "mongoose";
+
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  tags: [
+    {
+      type: String,
+    },
+  ],
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  published: {
+    type: Boolean,
+    default: false,
+  },
+  timestamps: true,
+});
+
+export const Blog = mongoose.model("Blog", blogSchema);
